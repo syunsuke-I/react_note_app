006d0ce02f2f13d7882888b6c09498254e9bc8f4
syunsuke-I <odol@ymail.ne.jp>
2023-11-18 23:42:10 +0900

    擬似追加

--- note-app/src/App.jsx
+++ note-app/src/App.jsx
@@ -6,10 +6,23 @@ import Sidebar from './components/Sidebar'
 
 function App() {
 
+  const [notes,setNotes] = useState([]);
+
+  const onAddNote = () =>{
+    const newNote = {
+      id:1,
+      title:"タイトル",
+      content:"内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes,newNote]);
+    console.log(notes)
+  }
+
   return (
     <>
       <div className="App">
-        <Sidebar/>
+        <Sidebar onAddNote={onAddNote} notes={notes}/>
         <Main/>
       </div>
     </>
--- note-app/src/components/Sidebar.jsx
+++ note-app/src/components/Sidebar.jsx
@@ -1,31 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Sidebar() {
-
+const Sidebar = ({ onAddNote, notes }) => {
   return (
     <>
       <div className="app-sidebar w-64 h-screen bg-gray-800 text-white p-4">
         <div className="header mb-6">
           <h1 className="text-xl font-semibold">ノート</h1>
-          <button className="mt-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-1 px-3 rounded">
+          <button 
+            className="mt-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-1 px-3 rounded"
+            onClick={onAddNote}
+          >
             追加
           </button>
         </div>
         <div className="notes space-y-4">
-          <div className="note bg-gray-700 p-3 rounded">
-            <div className="title flex justify-between items-center mb-2">
-              <strong>タイトル</strong>
-              <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-3 rounded">
-                削除
-              </button>
-            </div>
-            <p>ノートの内容</p>
-            <small className="text-gray-400">最後の修正日: xx/xx</small>
+          {notes.map((note)=>{
+            return(
+            <div key={note.id} className="note bg-gray-700 p-3 rounded">
+              <div className="title flex justify-between items-center mb-2">
+                <strong>{note.title}</strong>
+                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-3 rounded">
+                  削除
+                </button>
+              </div>
+              <p>{note.content}</p>
+              <small className="text-gray-400">最後の修正日: 
+              {new Date(note.modDate).toLocaleDateString("ja-Jp",
+              {
+                hour:"2-digit",
+                minute:"2-digit",
+              })}</small>
           </div>
+          );
+          })}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
+
+Sidebar.propTypes = {
+  onAddNote: PropTypes.func.isRequired,
+  
+};
 
 export default Sidebar
